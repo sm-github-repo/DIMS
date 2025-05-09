13c25a1559bc95c445e019c0d1d7ad1e3597126f
Sébastien Monsant <sebastien.monsant@gmail.com>
2025-05-09 18:35:47 +0200

    dims0.1

--- dims/dims/asgi.py
+++ dims/dims/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for entrepot project.
+ASGI config for dims project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'entrepot.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dims.settings')
 
 application = get_asgi_application()
